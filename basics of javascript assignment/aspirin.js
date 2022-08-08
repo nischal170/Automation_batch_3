a8fd01e26d4b4be9c80419882d47e62bfbf20f1b
prajwala12 <93109268+prajwala12@users.noreply.github.com>
2022-08-08 17:36:16 +0545

    Delete basics of javascript assignment directory
    mistakely uploaded

--- basics of javascript assignment/aspirin.js
+++
@@ -1,3 +0,0 @@
-let jsonData = require("./assignment.json");
-let name = jsonData['problems'][0]['Diabetes'][0]['medications'][0]['medicationsClasses'][0]['className'][0]['associatedDrug'][0]['name']
-console.log(name)
