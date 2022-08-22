9d98f61bc6fc761cb982cd38696d02d69acbc487
Nischal <nischalneupane17@gmail.com>
2022-08-22 11:44:22 +0545

    assignment 11 done

---
+++ 11.Implemention of session in login and pass data variables/cypress/integration/login/login.spec.js
@@ -0,0 +1,47 @@
+
+Given ("Url Visit",()=>{
+    cy.visit("https://koklassv4-orderingdev-ms.ekbana.net/system/login")
+})
+And ('inputvaliddata',(table)=>{
+    table.hashes().forEach(element=>{
+        cy.session([element.username,element.password],()=>{
+            cy.visit("https://koklassv4-orderingdev-ms.ekbana.net/system/login")
+            cy.get("input[placeholder='Enter Your Username']").type(element.username).should("have.value",element.username)
+            cy.get("input[placeholder='Enter your Password']").type(element.password).should("have.value",element.password)
+            cy.get("button[class='btn btn-default btn-login']").click()
+    
+        })
+        cy.visit("https://koklassv4-orderingdev-ms.ekbana.net/system/login")
+        
+
+    });
+})
+
+And ('input dataa {string} and {string}',(username,password)=>{
+    cy.session([username,password],()=>{
+        cy.visit("https://koklassv4-orderingdev-ms.ekbana.net/system/login")
+        cy.get("input[placeholder='Enter Your Username']").type(username).should("have.value",username)
+        cy.get("input[placeholder='Enter your Password']").type(password).should("have.value",password)
+        cy.get("button[class='btn btn-default btn-login']").click()
+
+    })
+    cy.visit("https://koklassv4-orderingdev-ms.ekbana.net/system/login")
+
+
+})
+
+And ('inputinvaliddata',(table)=>{
+    table.hashes().forEach(element=>{
+        cy.session([element.username,element.password],()=>{
+            cy.visit("https://koklassv4-orderingdev-ms.ekbana.net/system/login")
+            cy.get("input[placeholder='Enter Your Username']").type(element.username).should("have.value",element.username)
+            cy.get("input[placeholder='Enter your Password']").type(element.password).should("have.value",element.password)
+            cy.get("button[class='btn btn-default btn-login']").click()
+    
+        })
+        cy.visit("https://koklassv4-orderingdev-ms.ekbana.net/system/login")
+        
+
+    });
+})
+
