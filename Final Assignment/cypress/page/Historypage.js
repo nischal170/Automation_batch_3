b6192a5c7858fa89ac0fb698ac732faa15762f7e
Nischal <nischalneupane17@gmail.com>
2022-09-26 14:06:29 +0545

    history page table header

--- Final Assignment/cypress/page/Historypage.js
+++ Final Assignment/cypress/page/Historypage.js
@@ -19,13 +19,23 @@ export class historypage{
     verifytableheaders(){
         var arr=[1,4,6,7]
         var i=0
+        var j=0
+        var headers=['S.N','Reference code','Description','Action']
         cy.get('.ant-table-thead th.ant-table-cell').each((element)=>{
             i=i+1
-            j=j+1
             if (i==arr[j]){
-                
+                expect(headers[j]).to.equal(element.text())
+                j=j+1
+
 
             }
+
+        })
+        var rem_headers=['Date','Type','Status']
+        var m=0
+        cy.get(".ant-table-thead th.ant-table-column-has-sorters .ant-table-column-title").each((element)=>{
+            expect(rem_headers[m]).to.equal(element.text())
+            m=m+1
 
         })
     }
